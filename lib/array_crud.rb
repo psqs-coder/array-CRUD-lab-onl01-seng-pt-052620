81fe29a781f23555c2798d6666a8b1c2ed2bf8a9
Bruce Parker <detailsharks@gmail.com>
2020-06-07 13:26:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,19 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  "am"
 end
 
 def retrieve_first_element_from_array(array)
