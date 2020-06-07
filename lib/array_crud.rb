6da2f0ce4b0aee2056815860f69bc93b680a01ae
Bruce Parker <detailsharks@gmail.com>
2020-06-07 13:32:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def add_element_to_end_of_array(array, element)
    ["arrays!"]
 end
 
-def add_element_to_start_of_array(array, element).unshift
+def add_element_to_start_of_array(array, element)
   ("wow")
 end
 
